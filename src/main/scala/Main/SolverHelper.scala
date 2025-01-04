e5479bbc5bfbc9376c68bf6a9270ef73ae5f8da1
ralphkalweit <r.kalweit@innfactory.de>
2025-01-04 08:42:10 +0100

    feat: elimination matrix

--- src/main/scala/Main/SolverHelper.scala
+++ src/main/scala/Main/SolverHelper.scala
@@ -1,21 +1,100 @@
 package Main
 
+import Main.SudokuIO.areEqual
+import Main.SudokuValidation.{
+  getSudokuBlocks,
+  getSudokuColumns,
+  getSudokuRows,
+  hasLogicalErrors,
+  isCompleteSudoku
+}
+
+import scala.annotation.tailrec
+
 object SolverHelper {
 
   def insertAtFirstBlank(
       sudoku: List[List[Option[Int]]],
       element: Int
   ): List[List[Option[Int]]] = {
-    if element > sudoku.length then return sudoku
-
-    val rowIndex = sudoku.indexWhere(_.contains(None))
-
-    if rowIndex < 0 then sudoku
+    if (element > sudoku.length) sudoku
     else {
-      val row = sudoku(rowIndex)
-      val firstNoneIdx = row.indexOf(None)
-      sudoku.updated(rowIndex, row.updated(firstNoneIdx, Some(element)))
-    }
+      val rowIndex = sudoku.indexWhere(_.contains(None))
 
+      if (rowIndex < 0) sudoku
+      else {
+        val row = sudoku(rowIndex)
+        val firstNoneIdx = row.indexOf(None)
+        sudoku.updated(rowIndex, row.updated(firstNoneIdx, Some(element)))
+      }
+    }
   }
+
+  def getEliminationMatrix(
+      sudoku: List[List[Option[Int]]]
+  ): List[List[Set[Int]]] = {
+    val fullSet = (1 to sudoku.length).toSet
+    sudoku.map(_.map {
+      case Some(number) => Set(number)
+      case None         => fullSet
+    })
+  }
+
+  def getSudokuFromEliminationMatrix(
+      eliminationMatrix: List[List[Set[Int]]]
+  ): List[List[Option[Int]]] = {
+    eliminationMatrix.map(_.map {
+      case set if set.size == 1 =>
+        Some(set.head)
+      case _ => None
+    })
+  }
+
+  def eliminateOneStep(
+      possibilities: List[List[Set[Int]]]
+  ): List[List[Set[Int]]] = {
+    type T = List[List[Set[Int]]]
+    val transformers = List[List[List[Set[Int]]] => List[List[Set[Int]]]](
+      getSudokuRows,
+      getSudokuColumns,
+      getSudokuBlocks
+    )
+
+    transformers.foldLeft(possibilities) { (current, transform) =>
+      transform(
+        transform(current).map(eliminateListFromAllSingles)
+      )
+    }
+  }
+
+  @tailrec
+  def eliminateRecursive(
+      possibilities: List[List[Set[Int]]]
+  ): List[List[Set[Int]]] = {
+    val next = eliminateOneStep(possibilities)
+    if (areEqual(next, possibilities)) possibilities
+    else eliminateRecursive(next)
+  }
+
+  def eliminateListFromAllSingles(list: List[Set[Int]]): List[Set[Int]] = {
+
+    val singles = list.collect {
+      case s if s.size == 1 => s.head
+    }.toSet
+    list.map(set => if (set.size > 1) set -- singles else set)
+  }
+
+  def eliminationMatrixHasOpenDecisions(
+      possibilities: List[List[Set[Int]]]
+  ): Boolean = {
+    !possibilities.forall(_.forall(_.size == 1))
+  }
+
+  def eliminationMatrixIsSolved(
+      possibilities: List[List[Set[Int]]]
+  ): Boolean = {
+    val sudoku = getSudokuFromEliminationMatrix(possibilities)
+    isCompleteSudoku(sudoku) && !hasLogicalErrors(sudoku)
+  }
+
 }
--- src/test/scala/SolverHelperTest.scala
+++ src/test/scala/SolverHelperTest.scala
@@ -1,5 +1,14 @@
-import Main.SolverHelper.insertAtFirstBlank
-import Main.SudokuIO.{getGrid, getString}
+import Main.SolverHelper.{
+  eliminateListFromAllSingles,
+  eliminateOneStep,
+  eliminateRecursive,
+  eliminationMatrixIsSolved,
+  getEliminationMatrix,
+  getSudokuFromEliminationMatrix,
+  insertAtFirstBlank
+}
+import Main.SudokuIO.{areEqual, getGrid, getString}
+import Main.SudokuValidation.{getSudokuBlocks, getSudokuFromSudokuBlocks}
 import org.scalatest.funsuite.AnyFunSuite
 
 class SolverHelperTest extends AnyFunSuite {
@@ -31,6 +40,93 @@ class SolverHelperTest extends AnyFunSuite {
     val act = getString(insertAtFirstBlank(getGrid(str), 80))
     // TODO maybe expect an error here
     assert(exp != act)
+  }
+
+  test("transform to and from elimination Matrix") {
+
+    List(
+      "_ _ _ _\n_ _ _ _\n_ _ _ _\n_ _ _ _",
+      "4 _ _ _\n_ _ _ _\n_ _ _ _\n_ _ _ _",
+      "1 2 3 4\n3 _ _ 2\n2 1 4 3\n4 3 2 1"
+    ).foreach { str =>
+      val grid = getGrid(str)
+      val eliminationMatrix = getEliminationMatrix(grid)
+      val actual = getSudokuFromEliminationMatrix(eliminationMatrix)
+      assert(areEqual(grid, actual))
+    }
+  }
+
+  test("inverse block retrieval") {
+    List(
+      "_ _ _ _\n_ _ _ _\n_ _ _ _\n_ _ _ _",
+      "4 _ _ _\n_ _ _ _\n_ _ _ _\n_ _ _ _",
+      "4 3 2 1\n_ _ _ _\n_ _ _ _\n_ _ _ _",
+      "1 2 3 4\n3 _ _ 2\n2 1 4 3\n4 3 2 1",
+      "5 _ _ _ _ 8 _ _ 3 _ _ _ _ _ _ 7\n_ 7 _ 1 _ _ _ 9 _ _ _ _ _ _ _ _\n_ _ _ _ 6 _ _ _ 5 _ _ _ _ 12 _ _\n_ 2 _ _ _ _ 6 _ _ _ _ _ _ _ _ _\n_ _ _ 3 _ 7 _ _ _ _ 9 _ _ _ _ _\n_ _ _ _ _ _ _ 4 _ _ _ _ _ 1 _ _\n12 _ _ _ _ _ _ _ 7 _ _ _ _ _ _ 6\n_ _ _ _ _ 5 _ _ _ _ _ _ 9 _ _ _\n_ _ _ _ _ _ _ _ _ _ _ 14 _ _ 6 _\n9 _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n_ 12 _ _ _ 6 _ _ _ _ _ _ _ _ _ _\n_ _ _ _ 5 _ _ _ 9 _ _ _ _ _ _ _\n_ _ 9 _ _ _ _ _ _ _ _ _ 8 _ _ _\n_ _ _ _ _ _ _ 7 _ _ 14 _ _ _ _ _\n_ _ _ _ _ 9 _ _ _ 13 _ _ _ _ 8 _\n6 _ _ _ _ _ _ _ _ _ _ _ _ _ _ 5",
+      "1 2 3 4 5 6 7 8 9\n4 5 6 7 8 9 1 2 3\n7 8 9 1 2 3 4 5 6\n2 3 4 5 6 7 8 9 1\n5 6 7 8 9 1 2 3 4\n8 9 1 2 3 4 5 6 7\n3 4 5 6 7 8 9 1 2\n6 7 8 9 1 2 3 4 5\n9 1 2 3 4 5 6 7 8"
+    ).foreach { str =>
+      val grid = getGrid(str)
+      val blocks = getSudokuBlocks(grid)
+      val actual = getSudokuFromSudokuBlocks(blocks)
+
+      assert(areEqual(grid, actual))
+    }
+  }
+
+  test("eliminateFromAllSingles should handle various cases correctly") {
+    val testCases = List(
+      List(Set(1, 2, 3), Set(1), Set(2, 4), Set(5)) -> List(
+        Set(2, 3),
+        Set(1),
+        Set(2, 4),
+        Set(5)
+      ),
+      List(Set(1, 2), Set(3, 4), Set(5, 6)) -> List(
+        Set(1, 2),
+        Set(3, 4),
+        Set(5, 6)
+      ),
+      List(Set(1), Set(2), Set(3)) -> List(Set(1), Set(2), Set(3)),
+      List(Set(1, 2, 3), Set(1), Set(1, 4), Set(5)) -> List(
+        Set(2, 3),
+        Set(1),
+        Set(4),
+        Set(5)
+      )
+    )
+
+    testCases.foreach { case (input, expectedOutput) =>
+      assert(eliminateListFromAllSingles(input) == expectedOutput)
+    }
+  }
+
+  test("eliminate one step") {
+    val smallSudoku = "1 2 3 4\n3 _ _ 2\n2 1 4 3\n4 3 2 1"
+    val expectedSolution = "1 2 3 4\n3 4 1 2\n2 1 4 3\n4 3 2 1"
+    val possibilities = getEliminationMatrix(getGrid(smallSudoku))
+    val eliminatedOnce = eliminateOneStep(possibilities)
+    assert(eliminationMatrixIsSolved(eliminatedOnce))
+    //
+    val result = getSudokuFromEliminationMatrix(eliminatedOnce)
+    val expectedResult = getGrid(expectedSolution)
+    assert(areEqual(result, expectedResult))
+  }
+
+  test("elimination tail recursive") {
+    val sudokuMap = Map(
+      "1 2 3 4\n3 _ _ 2\n2 1 4 3\n4 3 2 1" -> "1 2 3 4\n3 4 1 2\n2 1 4 3\n4 3 2 1"
+    )
+    sudokuMap.foreach { case (inputSudoku, expectedSolution) =>
+      val poss = getEliminationMatrix(getGrid(inputSudoku))
+      val eliminatedResult = eliminateRecursive(poss)
+      assert(eliminationMatrixIsSolved(eliminatedResult))
+
+      val result = getSudokuFromEliminationMatrix(eliminatedResult)
+      val expectedResult = getGrid(expectedSolution)
+      assert(
+        areEqual(result, expectedResult)
+      )
+    }
   }
 
 }
